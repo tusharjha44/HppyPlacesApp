74d59096502aefcbd97b93930976fa6473cfe506
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2021-12-14 17:26:34 +0530

    Initial Commit. Added main and add happy place activity

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,26 @@
+package com.example.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding : ActivityAddHappyPlaceBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.happyplaces
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlaces?.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
